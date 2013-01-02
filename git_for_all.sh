7d37e0271276e9c6548c2ce311085f18f13d8539
Piotr Mika <piotr.mt.mika@gmail.com>
2013-01-02 15:05:32 +0100

    No Check in git-for-all: `NC' in dir name causes no checking

--- git_for_all.sh
+++ git_for_all.sh
@@ -6,48 +6,51 @@ col=32
 cd /home/insofter/projects
 for x in * 
 do
-  if [ -d "${x}/.git" ]
-  then 
-    cd ${x}
-    echo 
-    echo -ne "\033[1;${col}m"
-    echo ">>> ${x} <<<"
-    echo -ne "\033[0;37m"
-    case "$1" in
+  if echo ${x} | grep -q -v NC
+  then
+    if [ -d "${x}/.git" ]
+    then 
+      cd ${x}
+      echo 
+      echo -ne "\033[1;${col}m"
+      echo ">>> ${x} <<<"
+      echo -ne "\033[0;37m"
+      case "$1" in
 
-      push)
-        if git status | grep  -q "nothing to commit"
-        then
-          git push
-        else
-          echo "--Commit ${x}!"
-        fi
+        push)
+          if git status | grep  -q "nothing to commit"
+          then
+            git push
+          else
+            echo "--Commit ${x}!"
+          fi
 
-        ;;
-      pull)
-        if git status | grep  -q "nothing to commit"
-        then
-          git pull
-        else
-          echo "--Commit ${x}!"
-        fi
+          ;;
+        pull)
+          if git status | grep  -q "nothing to commit"
+          then
+            git pull
+          else
+            echo "--Commit ${x}!"
+          fi
 
-        ;;
+          ;;
 
-      *)
-        git status
+        *)
+          git status
 
-        ;;
+          ;;
 
-    esac
-    cd ..
-    if [ $col -ge 36 ]
-    then
-      col=32
-    else
-      col=$[ $col+1 ]
+      esac
+      cd ..
+      if [ $col -ge 36 ]
+      then
+        col=32
+      else
+        col=$[ $col+1 ]
+      fi
+
     fi
-
   fi
 done
 
