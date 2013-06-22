dfb889c8f58d4a09c98fb51dfe6c1d8d896b0303
Piotr Mika <mail@server.com>
2013-06-22 17:02:05 +0200

    mod test to CounterThic

--- test_icd-counters-daemon.sh
+++ test_icd-counters-daemon.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
-if [ "$1" = "--write" ]; then #writing events to pipe
-  
-  sleep 1
-  echo `date +%s` 0 1
-  sleep 1
-  echo `date +%s` 0 0
-  sleep 1
-  echo `date +%s` 0 1
-  sleep 10
-  echo `date +%s` 0 0
+case  "$1" in 
 
+  "--write0")
+    #writing events to pipe
 
-  sleep 100 #keep pipe alive
-else #run test
+    sleep 1
+    echo `date +%s` 0 1
+    sleep 2
+    echo `date +%s` 0 0
+    sleep 2
+    echo `date +%s` 0 1
+    sleep 2
+    echo `date +%s` 0 0
+    sleep 100 #keep pipe alive
+    
+    ;;
 
-  rm -rf /tmp/itd
-  mknod /tmp/itd p
+  "--write1")
 
-  $0 --write >> /tmp/itd &
-  cd ${ICDTCP3_DIR}/local_icd_compilation
-  src/icd-counters-daemon 
+    sleep 1
+    echo `date +%s` 0 1
+    sleep 45
+    echo `date +%s` 0 0
+    sleep 100 #keep pipe alive
+    
+    ;;
 
-fi
+  *)
+    rm -rf /tmp/itd0
+    mknod /tmp/itd0 p
+    rm -rf /tmp/itd1
+    mknod /tmp/itd1 p
+
+    $0 --write0 >> /tmp/itd0 &
+    $0 --write1 >> /tmp/itd1 &
+
+    cd ${ICDTCP3_DIR}/local_icd_compilation
+    src/icd-counters-daemon 
+    ;;
+
+esac
+
